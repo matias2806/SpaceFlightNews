9bf840c167ee10f9d45e23a5c5c64c85a3243636
Matias Palmieri <matias.palmieri@uala.com.ar>
2026-04-06 21:19:35 -0300

    initial commit

---
+++ SpaceFlightNews/SpaceFlightNews/SpaceFlightNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceFlightNewsApp.swift
+//  SpaceFlightNews
+//
+//  Created by Matias Palmieri on 06.04.26.
+//
+
+import SwiftUI
+
+@main
+struct SpaceFlightNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
